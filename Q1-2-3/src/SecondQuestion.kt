da67d97cf298c7998b50b390a17fd56e456e1daf
hlandim <hlandim.s@gmail.com>
2018-06-22 15:00:07 -0300

    Creating second question solution

--- Q1-2-3/src/SecondQuestion.kt
+++ Q1-2-3/src/SecondQuestion.kt
@@ -1,6 +1,50 @@
 /**
  * Created by hlandim on 22/06/18.
  */
+
 fun main(args: Array<String>) {
+
+    checkPartialPermutation("you", "yuo")
+    checkPartialPermutation("probably", "porbalby")
+    checkPartialPermutation("despite", "desptie")
+    checkPartialPermutation("moon", "nmoo")
+    checkPartialPermutation("misspellings", "mpeissngslli")
+
+}
+
+fun checkPartialPermutation(first: String, second: String) {
+
+    print("$first, $second -> ")
+    println(isPartialPermutation(first, second))
     
+}
+
+fun isPartialPermutation(first: String, second: String): Boolean {
+
+    if (second.length == first.length) {
+
+        if (first[0] == second[0]) {
+
+            val wordsLength = second.length
+
+            var differentLettersCount = 0
+
+            first.forEachIndexed { index, c ->
+                if (second[index] != c) {
+                    differentLettersCount++
+                }
+            }
+
+            if (wordsLength > 3) {
+                if (differentLettersCount < (wordsLength * (2f / 3f))) {
+                    return true
+                }
+            } else {
+                return differentLettersCount > 0
+            }
+
+        }
+    }
+
+    return false
 }
